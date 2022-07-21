434e4725743be7e14addc310a9ba00fc1d140ee6
Jaquedonin <jdonin@eng.ci.ufpb.br>
2022-07-21 14:03:57 -0300

    Third commit

--- src/First.java
+++ src/First.java
@@ -5,6 +5,7 @@ public class First {
 		String variavel = "O objetivo deste repositório é finalizar o primeiro desafio de projeto do bootcamp Santander Code Girls";
 		System.out.println(variavel);
 		String alteracoes = "Segundo commit para exemplificar merge request.";
+		System.out.println(alteracoes);
 	}
 
 }
