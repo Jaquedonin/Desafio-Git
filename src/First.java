46be2e4b2bfdca5ac0cf0ad3792dc2554c98af76
Jaquedonin <jdonin@eng.ci.ufpb.br>
2022-07-21 13:55:22 -0300

    First commit to create repository and push files.

---
+++ src/First.java
@@ -0,0 +1,9 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		String variavel = "O objejetivo deste repositório é finalizar o primeiro desafio de projeto do bootcamp Santander Code Girls";
+		System.out.println(variavel);
+	}
+
+}
